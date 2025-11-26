df14c0b6b631d2df384b6ca7de68ad2d7880520a
Tomek <tomek@japko.info>
2025-11-26 23:12:46 +0100

    Add Bootstrap and give web UI some polish

--- frontend/webapp/src/routes/+layout.svelte
+++ frontend/webapp/src/routes/+layout.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-	import favicon from '../lib/assets/favicon.svg';
+    import favicon from '../lib/assets/favicon.svg';
 
-	let { children } = $props();
+    import {Container, Nav, Navbar, NavbarBrand, NavItem, NavLink} from '@sveltestrap/sveltestrap';
+
+    let {children} = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon}/>
 </svelte:head>
 
-{@render children()}
+<Navbar color="dark" dark expand class="px-3">
+    <NavbarBrand href="/">Boilerplate</NavbarBrand>
+    <Nav navbar pills class="me-auto">
+        <NavItem>
+            <NavLink href="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink href="/about">About</NavLink>
+        </NavItem>
+    </Nav>
+</Navbar>
+<Container fluid class="pt-2">
+    {@render children()}
+</Container>
--- frontend/webapp/src/routes/+page.svelte
+++ frontend/webapp/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { fetchCurrentTime } from '../lib/services/hello';
+    import {Button, Col, Row} from "@sveltestrap/sveltestrap";
 
 	let currentTime = $state<string>('');
 	let loading = $state<boolean>(true);
@@ -18,14 +19,27 @@
 	});
 </script>
 
-<h1>Welcome to Golang-SvelteKit Boilerplate</h1>
+<Row>
+    <h1>Welcome to Golang-SvelteKit Boilerplate</h1>
+</Row>
+<Row class="pt-4">
+    <Col md="2">
+        Get time from backend:
+    </Col>
+    <Col md="9">
+        {#if loading}
+            <p>Loading...</p>
+        {:else if error}
+            <p>Error: {error}</p>
+        {:else}
+            <p>Now is {currentTime}</p>
+        {/if}
+    </Col>
+</Row>
 
-{#if loading}
-	<p>Loading...</p>
-{:else if error}
-	<p>Error: {error}</p>
-{:else}
-	<p>Now is {currentTime}</p>
-{/if}
-
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Row class="bg-secondary-subtle">
+    <div class="p-3">
+        Made with <a href="https://svelte.dev/docs/kit"><b>Svelte</b></a>
+            and <a href="https://sveltestrap.js.org/?path=/docs/sveltestrap-overview--docs"><b>Sveltestrap</b></a> (Bootstrap CSS components).
+    </div>
+</Row>
